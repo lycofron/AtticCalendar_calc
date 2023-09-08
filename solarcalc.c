3957b4a481aa5a91303b06749a5ba07cb0f02240
Ioannis Theodossis <ioannis.theodossis@openbet.om>
2023-09-08 19:21:54 +0300

    Calc apparent solstice

--- solarcalc.c
+++ solarcalc.c
@@ -13,7 +13,7 @@ const double ATHENS_MEAN_HUMIDITY = 40.0;
 double ATHENS_ATMOSPHERE[4] = {ATHENS_MEAN_ATPRESSURE, ATHENS_MEAN_TEMPERATURE, ATHENS_MEAN_HUMIDITY, 0};
 double VOID_OBSERVER[] = {36,1,0,0,0,0};
 
-double summerSolsticePoint = 90.0;
+const double summerSolsticePoint = 90.0;
 
 void printJulianDate(char * msg, double jd){
     int jday, jmon, jyear, jhour, jmin, jsec;
@@ -39,7 +39,7 @@ double getNextSolstice(double jd) {
     double solsticejd;
     char serr[AS_MAXCH];
 
-    solsticejd = swe_solcross(summerSolsticePoint, jd, iflag, serr);
+    solsticejd = swe_solcross_ut(summerSolsticePoint, jd, iflag, serr);
     if (solsticejd < jd) {
       printf("error: %s\n", serr);
     }
@@ -97,6 +97,42 @@ void calc_next_sunset(double tjd_ut, double *darr) {
   }
 }
 
+double getApparentSolsticeDate(double jd_initial){
+
+  // char dateLabel[] = "Sun setting at:";
+  // char degreeLabel[] = "\tAzimuth: ";
+  double darr[50] = { 0 };
+  const int DAYS_AROUND_SOLSTICE = 3;
+  const int CONSECUTIVE_DAYS = DAYS_AROUND_SOLSTICE * 2 +1;
+  struct time_azimuth {
+    double sunset_time;
+    double sun_azimuth;
+  };
+  struct time_azimuth consecutive_sunsets[CONSECUTIVE_DAYS];
+
+  // printf("Longitude\tlatitude\tdistance\tspeed long.\tspeed lat.\n");
+  double jd;
+  int i = 0;
+  for(jd=jd_initial - DAYS_AROUND_SOLSTICE; jd<=jd_initial+DAYS_AROUND_SOLSTICE; jd=jd+0.5){
+    calc_next_sunset(jd, darr);
+    jd=darr[22];
+    consecutive_sunsets[i].sunset_time=darr[22];
+    consecutive_sunsets[i].sun_azimuth=darr[5];
+    i++;
+  }
+
+  for(i=1; i<CONSECUTIVE_DAYS-1; i++){
+    if (consecutive_sunsets[i].sun_azimuth < consecutive_sunsets[i+1].sun_azimuth &&
+    consecutive_sunsets[i].sun_azimuth < consecutive_sunsets[i+1].sun_azimuth) {
+      return consecutive_sunsets[i].sunset_time;
+    }
+    // printJulianDate(dateLabel, consecutive_sunsets[i].sunset_time);
+    // printDegree(degreeLabel, consecutive_sunsets[i].sun_azimuth);
+  }
+
+  return 0.0;
+
+}
 
 int main()
 {
@@ -108,40 +144,29 @@ int main()
     int startday = 1, startmon = 1, startyear = 2020;
     double startut = 0.0;
 
-    // int stopday = 31, stopmon = 12, stopyear = 2030;
-    // double stoput = 0.0;
+    int stopday = 31, stopmon = 12, stopyear = 2030;
+    double stoput = 0.0;
 
     double startdt = swe_julday(startyear,startmon,startday,startut,SE_GREG_CAL);
     startdt = startdt + swe_deltat(startdt);
-    // double stopjdt = swe_julday(stopyear,stopmon,stopday,startut,SE_GREG_CAL);
-    // stopjdt = stopjdt + swe_deltat(stopjdt);
+    double stopjdt = swe_julday(stopyear,stopmon,stopday,stoput,SE_GREG_CAL);
+    stopjdt = stopjdt + swe_deltat(stopjdt);
 
     /* Working-storage section */
 
     // double x2[6];
-    char dateLabel[] = "Sun setting at:";
-    char degreeLabel[] = "\tAzimuth: ";
 
     // Get apparent solstice date
-    double true_solstice = getNextSolstice(startdt);
-    double next_sunset = 0.0;
-    double sun_azimuth = 0.0;
-    double darr[50] = { 0 };
-    const int ZONE_DELTA = 2;
-
-    printf("Longitude\tlatitude\tdistance\tspeed long.\tspeed lat.\n");
-    double jd;
-    jd=true_solstice - ZONE_DELTA;
-    while(jd<=true_solstice+ZONE_DELTA){
-      calc_next_sunset(jd, darr);
-      next_sunset=darr[22];
-      sun_azimuth=darr[5];
-      printJulianDate(dateLabel, next_sunset);
-      printDegree(degreeLabel, sun_azimuth);
-      jd=next_sunset+0.5;
+    char true_solstice_label[] = "\nTrue Solstice date:";
+    char apparent_solstice_label[] = "Apparent Solstice date:";
+    double current_jdt = startdt;
+    while(current_jdt< stopjdt) {
+      double true_solstice = getNextSolstice(current_jdt);
+      double apparent_solstice = getApparentSolsticeDate(true_solstice);
+      printJulianDate(true_solstice_label, true_solstice);
+      printJulianDate(apparent_solstice_label, apparent_solstice);
+      current_jdt = current_jdt + 360;
     }
-    // printf("%11.7f\t%10.7f\t%10.7f\t%10.7f\t%10.7f\n",
-    //  x2[0], x2[1], x2[2], x2[3], x2[4]);
 
   return OK;
 }
